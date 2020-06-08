0d32179d4b20da0e8cdea24c844563351919cd93
Giacomo Caironi <giacomo.caironi@gmail.com>
2020-06-08 09:40:30 +0200

    Modify _mult_jac

--- btclib/curvemult2.py
+++ btclib/curvemult2.py
@@ -19,6 +19,52 @@ from .curves import secp256k1
 from .utils import int_from_integer
 
 
+def _unsafe_add_jac(Q: JacPoint, R: JacPoint, ec: Curve = secp256k1) -> JacPoint:
+    # R and Q must be different
+
+    RZ2 = R[2] * R[2]
+    RZ3 = RZ2 * R[2]
+    QZ2 = Q[2] * Q[2]
+    QZ3 = QZ2 * Q[2]
+    if Q[0] * RZ2 % ec.p == R[0] * QZ2 % ec.p:
+        if Q[1] * RZ3 % ec.p != R[1] * QZ3 % ec.p:
+            return INFJ
+
+    if Q[2] == 0 or R[2] == 0:  # Infinity point in Jacobian coordinates
+        return R
+
+    T = (Q[1] * RZ3) % ec.p
+    U = (R[1] * QZ3) % ec.p
+    W = (U - T) % ec.p
+
+    M = (Q[0] * RZ2) % ec.p
+    N = (R[0] * QZ2) % ec.p
+    V = (N - M) % ec.p
+
+    V2 = V * V
+    V3 = V2 * V
+    MV2 = M * V2
+    X = (W * W - V3 - 2 * MV2) % ec.p
+    Y = (W * (MV2 - X) - T * V3) % ec.p
+    Z = (V * Q[2] * R[2]) % ec.p
+    return X, Y, Z
+
+
+def _double_jac(Q: JacPoint, ec: Curve = secp256k1) -> JacPoint:
+
+    if Q[2] == 0:
+        return INFJ
+
+    QZ2 = Q[2] * Q[2]
+    QY2 = Q[1] * Q[1]
+    W = (3 * Q[0] * Q[0] + ec._a * QZ2 * QZ2) % ec.p
+    V = (4 * Q[0] * QY2) % ec.p
+    X = (W * W - 2 * V) % ec.p
+    Y = (W * (V - X) - 8 * QY2 * QY2) % ec.p
+    Z = (2 * Q[1] * Q[2]) % ec.p
+    return X, Y, Z
+
+
 def _mult_jac(m: int, Q: JacPoint, ec: CurveGroup) -> JacPoint:
     """Scalar multiplication of a curve point in Jacobian coordinates.
 
@@ -41,9 +87,9 @@ def _mult_jac(m: int, Q: JacPoint, ec: CurveGroup) -> JacPoint:
     R = INFJ  # initialize as infinity point
     while m > 0:  # use binary representation of m
         if m & 1:  # if least significant bit is 1
-            R = ec._add_jac(R, Q)  # then add current Q
+            R = _unsafe_add_jac(R, Q, ec)  # then add current Q
         m = m >> 1  # remove the bit just accounted for
-        Q = ec._add_jac(Q, Q)  # double Q for next step
+        Q = _double_jac(Q, ec)  # double Q for next step
     return R
 
 
